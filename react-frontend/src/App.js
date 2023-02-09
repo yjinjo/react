28aa1d862910a51276e97e9ba9c055660a701f35
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-09 12:44:11 +0900

    #1. Ant Design Button Example

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,32 @@
 import logo from "./logo.svg";
 import { Button } from 'antd';
+import React from 'react';
 import "./App.css";
+
+class Counter1 extends React.Component {
+  state = {
+    value: this.props.initialValue
+  }
+
+  onClick = () => {
+    const { value } = this.state;
+    this.setState({ value: value + 1 });
+  }
+
+  render() {
+    const { value } = this.state;  // 현재의 상탯값을 참조
+    return (
+      <div>
+        Counter1: { value }
+        <Button onClick={this.onClick}>+1</Button>
+      </div>
+    ); // jsx 문법
+  }
+}
 
 function App() {
   return <div>
-    <Button type="primary" onClick={ () => console.log("clicked") }>
-      Hello, Antd.
-    </Button>
+    <Counter1 initialValue={ 10 } />
   </div>;
 }
 
