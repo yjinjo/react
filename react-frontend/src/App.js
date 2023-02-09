a1cec965a5778674f9f4a320cb6f814c95f725df
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-09 12:46:54 +0900

    #1. 클래스형 Component 하드코딩

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -24,9 +24,25 @@ class Counter1 extends React.Component {
   }
 }
 
+class FruitComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>좋아하는 과일</h1>
+        <ul>
+          <li>바나나</li>
+          <li>사과</li>
+          <li>딸기</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
 function App() {
   return <div>
     <Counter1 initialValue={ 10 } />
+    <FruitComponent />
   </div>;
 }
 
