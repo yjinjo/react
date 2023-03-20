c85162f5522b260c825aad5a502e398e8c3ac717
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-20 11:50:41 +0900

    #9. Initialization

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `Total: ${coins.length}`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <ul>
-          {coins.map((coin) => (
-            <li key={coin.id}>
-              {coin.name} ({coin.symbol}: ${coin.quotes["USD"].price} USD
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
