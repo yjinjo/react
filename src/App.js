9f78a448a29b4eab2ee199ae12969b84cde4ded1
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-20 11:48:18 +0900

    #8. Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>The Coins! {loading ? "" : `Total: ${coins.length}`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <ul>
+          {coins.map((coin) => (
+            <li key={coin.id}>
+              {coin.name} ({coin.symbol}: ${coin.quotes["USD"].price} USD
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default App;
